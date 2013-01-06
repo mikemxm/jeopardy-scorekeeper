2ab5bad5fe4519baf54dce1ed26b0292fbe297e9
Mike Malinowski <stinky613@rMBP.local>
2013-01-06 00:19:42 -0500

    Trying to implement haml

--- index.rb
+++ index.rb
@@ -1,18 +1,5 @@
 #!/usr/bin/ruby
 
-puts "Content-Type: text/html\n\n"
+require 'haml'
 
-puts """
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Jeopary Scorekeeper</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-	</head>
-	<body>
-		<h1>Jeopardy Scorekeeper</h1>
-		<script src="//code.jquery.com/jquery-latest.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-	</body>
-</html>
-""""
+Haml::Engine.new(File.open("index.html.haml")).render
