a76456223741823ed1287ce4e6145dd97f5ea4c1
Mike Malinowski <stinky613@rMBP.local>
2013-01-05 23:41:30 -0500

    Fixing 500 error

--- index.rb
+++ index.rb
@@ -1,1 +1,18 @@
-<!doctype html>
+#!/usr/bin/ruby
+
+puts "Content-Type: text/html\n\n"
+
+puts """
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Jeopary Scorekeeper</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+	<body>
+		<h1>Jeopardy Scorekeeper</h1>
+		<script src="//code.jquery.com/jquery-latest.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
+""""
