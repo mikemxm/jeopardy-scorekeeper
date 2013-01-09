43354164e3344e0bf7837c6a3d3b50f4f5c6d13b
Mike Malinowski <stinky613@rMBP.local>
2013-01-09 12:11:39 -0500

    Basic score updating to test

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,4 +1,24 @@
-var sj_score = 0, dj_score = 0, total_score = 0
+var sj_score = 0, dj_score = 0, total_score = 0;
+
+function update_totals() {
+
+	sj_score = 0, dj_score = 0, total_score = 0;
+
+	sj_score = sum_of_form("sj")
+	dj_score = sum_of_form("dj")
+	total_score = sj_score + dj_score;
+	console.log(sj_score)
+	console.log(dj_score)
+	console.log(total_score)
+}
+
+function sum_of_form(form_id) {
+	var running_total = 0
+	$("form#" + form_id + " input").each(function() {
+		running_total += this.value
+	});
+	return running_total
+}
 
 $('label').mousedown(function() {
 	$(this).addClass("pressed")
@@ -6,4 +26,8 @@ $('label').mousedown(function() {
 
 $('label').mouseup(function() {
 	$(this).removeClass("pressed")
+});
+
+$('label').click(function() {
+	update_totals()
 });
