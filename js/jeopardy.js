255fe1f62c63b9f8ce76d93b06e9c8b4900f42bb
Mike Malinowski <stinky613@rMBP.local>
2013-01-09 11:48:05 -0500

    Forgot jquery selector around this. Fixed.

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,9 +1,9 @@
 var sj_score = 0, dj_score = 0, total_score = 0
 
 $('label').mousedown(function() {
-	this.addClass("pressed")
+	$(this).addClass("pressed")
 });
 
 $('label').mouseup(function() {
-	this.removeClass("pressed")
+	$(this).removeClass("pressed")
 });
